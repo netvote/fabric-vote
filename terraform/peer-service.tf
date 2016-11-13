f0a6fa5822fdff9d3f9bc52cb7f922c254052c7d
slanders <slanders@pindrop.com>
2016-11-12 20:45:48 -0500

    adding peer node terraform

---
+++ terraform/peer-service.tf
@@ -0,0 +1,68 @@
+# peer SERVICES
+resource "aws_ecs_task_definition" "peer" {
+  family = "peer"
+  container_definitions = "${file("tasks/peer.json")}"
+}
+
+resource "aws_ecs_service" "peer" {
+  name = "peer"
+  cluster = "${aws_ecs_cluster.netvote.id}"
+  task_definition = "${aws_ecs_task_definition.peer.arn}"
+  desired_count = 1
+  iam_role = "${aws_iam_role.ecs_instance_role.arn}"
+
+  load_balancer {
+    elb_name = "${aws_elb.peer.name}"
+    container_name = "peer"
+    container_port = 7050
+  }
+}
+
+resource "aws_route53_record" "peer" {
+  zone_id = "ZTO1AJYOEZG73"
+  name = "peer"
+  type = "CNAME"
+  records = ["${aws_elb.peer.dns_name}"]
+  ttl = "30"
+}
+
+resource "aws_elb" "peer" {
+  name = "peer-service-elb"
+  availability_zones = ["us-east-1a","us-east-1c","us-east-1d"]
+
+  listener {
+    instance_port = 7050
+    instance_protocol = "tcp"
+    lb_port = 7050
+    lb_protocol = "tcp"
+  }
+
+  listener {
+    instance_port = 9051
+    instance_protocol = "tcp"
+    lb_port = 7051
+    lb_protocol = "tcp"
+  }
+
+  listener {
+    instance_port = 9052
+    instance_protocol = "tcp"
+    lb_port = 7052
+    lb_protocol = "tcp"
+  }
+
+  listener {
+    instance_port = 9375
+    instance_protocol = "tcp"
+    lb_port = 2375
+    lb_protocol = "tcp"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 10
+    timeout = 20
+    target = "TCP:7050"
+    interval = 30
+  }
+}
