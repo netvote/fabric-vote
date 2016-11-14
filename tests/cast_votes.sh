f0c4a67ebf2addfd01da2c02c463a87ab0f4d960
slanders <slanders@pindrop.com>
2016-11-13 21:45:27 -0500

    test curls

---
+++ tests/cast_votes.sh
@@ -0,0 +1,1 @@
+curl -X POST -H "x-api-key: IZBPwCePaQ13kA9o4wQ70aiC7vvrLXIE2m6mKrlw" -H "Content-Type: application/json" --data @votes.json https://api.stevenlanders.com/dev/vote/erin
---
+++ tests/get_ballot.sh
@@ -0,0 +1,1 @@
+curl -s -H "x-api-key: IZBPwCePaQ13kA9o4wQ70aiC7vvrLXIE2m6mKrlw" https://api.stevenlanders.net/dev/ballot/erin |jq
