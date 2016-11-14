62951a2706fccbfd123148e53bf4ad3e7085d74a
slanders <slanders@pindrop.com>
2016-11-13 21:49:24 -0500

    test curls

--- tests/cast_votes.sh
+++ tests/cast_votes.sh
@@ -1,1 +1,1 @@
-curl -X POST -H "x-api-key: IZBPwCePaQ13kA9o4wQ70aiC7vvrLXIE2m6mKrlw" -H "Content-Type: application/json" --data @votes.json https://api.stevenlanders.com/dev/vote/erin
+curl -X POST -H "x-api-key: IZBPwCePaQ13kA9o4wQ70aiC7vvrLXIE2m6mKrlw" -H "Content-Type: application/json" --data @votes.json https://9cylao0on7.execute-api.us-east-1.amazonaws.com/dev/vote/erin
