d035ddbc800ed1dc73ac97754053574a1da0fe6e
slanders <slanders@pindrop.com>
2016-11-17 09:57:43 -0500

    renaming terraform to make room for non-ecs version

--- ecs-terraform/ssh_ecs.sh
+++ ecs-terraform/ssh_ecs.sh
@@ -1,5 +1,5 @@
 # basic script to aid ssh
 
-node_ip=`terraform show |grep public_ip | sed -e "${1}q;d" |sed 's/.*= //g'`
+node_ip=`ecs-terraform show |grep public_ip | sed -e "${1}q;d" |sed 's/.*= //g'`
 
 ssh -oStrictHostKeyChecking=no ec2-user@$node_ip -i ~/.ssh/netvote-slanders.pem
